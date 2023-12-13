ae309b503507f1a6098db4363067482621284efe
mahde13 <musavemahdy>
2023-12-13 06:43:31 -0900

    Animation side navigation with js

--- main.js
+++ main.js
@@ -3,9 +3,7 @@ const icon_bar = document.querySelector(".open-nav-icon");
 const panel = document.querySelector(".content-panel");
 const close = document.querySelector(".close-nav");
 icon_bar.addEventListener("click", (e) => {
-  // nav.classList.toggle("openNav");
-  nav.style.display = "block";
-  nav.style.width = "20%";
+  nav.classList.toggle("openNav");
 });
 close.addEventListener("click", () => {
   nav.style.display = "none";
