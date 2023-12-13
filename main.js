7774dbf1759748e231be516af7bb6b3a61384cc0
mahde13 <musavemahdy>
2023-12-13 07:42:42 -0900

    create animation with side navbar

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const nav = document.querySelector(".sidebar");
 const iconBar = document.querySelector(".open-nav-icon");
-const panel = document.querySelector(".content-panel");
 const close = document.querySelector(".close-nav");
 
 iconBar.addEventListener("click", (e) => {
