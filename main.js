6493626a06bf41f875f06ecb54ecb6f5ffda0c28
mahde13 <musavemahdy>
2023-12-13 07:33:32 -0900

    animation side navbar with with and height

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ const panel = document.querySelector(".content-panel");
 const close = document.querySelector(".close-nav");
 icon_bar.addEventListener("click", (e) => {
   nav.style.width = "100%";
-  // nav.style.left = 0;
 });
 close.addEventListener("click", () => {
   nav.style.width = "0%";
-  // nav.style.left = "-100%";
 });
