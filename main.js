cfb46dfadffc8b122a445b4abcc08dfabf5a5df6
mahde13 <musavemahdy>
2023-12-13 21:19:46 -0900

    create a vertical_menu navbar

--- main.js
+++
@@ -1,7 +0,0 @@
-const nav = document.querySelector(".sidebar");
-const icon_bar = document.querySelector(".open-nav-icon");
-const panel = document.querySelector(".content-panel");
-const close = document.querySelector(".close-nav");
-icon_bar.addEventListener("click", (e) => {
-  nav.classList.toggle("openNav");
-});
